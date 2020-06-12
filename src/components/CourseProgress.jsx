772fd8a097927f268e807e9b18d61b5dd9d8708e
dpandya <dv691600@dal.ca>
2020-06-12 01:05:10 -0300

    added logo

--- src/components/CourseProgress.jsx
+++ src/components/CourseProgress.jsx
@@ -2,10 +2,24 @@ import React, { Component } from 'react'
 import '.././stylesheets/ProgressBarStyle.css'
 
 class CourseProgress extends Component {
+    // setprgval = () => {
+    //     document.getElementsByClassName('badge').style.display = "block";
+    // }
+    newStyle = {
+        display: 'block'
+    };
+    showval = () => {
+        document.getElementsByClassName('badge').style = this.newStyle;
+        console.log('Mouse enter');
+        // console.log(document.getElementsByClassName('badge'));
+    }
     render() {
         return (
-            <div className="progress">
-                <div className="progress-bar bg-success" style={{width: this.props.prgval + "%"}}></div>
+            <div>
+                <div className="badge badge-grey" style={{position: "relative", right: this.props.prgval + "%", display: "hidden"}}>{this.props.prgval}</div>
+                <div className="progress" onMouseOver={this.showval}>
+                    <div className="progress-bar bg-success" style={{ width: this.props.prgval + "%" }}></div>
+                </div>
             </div>
         )
     }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import logo from '../images/logo192.png'
 
 const NavigationBar = () => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow" style={{ position: "fixed", top: "0px", width: "100%", zIndex: 100 }}>
+        <img src={logo} width="30" height="30" alt="logo" style={{marginRight: "1rem"}}></img>
         <a className="navbar-brand" href="#">stacklearner</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
