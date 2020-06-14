d4062362109f6ab659fd594f1ebb0afd44bcb4c5
dpandya <dv691600@dal.ca>
2020-06-14 15:13:41 -0300

    added form validations

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -11,27 +11,66 @@ class Signup extends Component {
 
         this.state = {
             email: "",
-            password: ""
+            password: "",
+            emailErrorMsg: "",
+            passwordErrorMsg: <div></div>,
         }
 
     }
+
+    validateEmail = (email) => {
+        if(email.length === 0) {
+            return ("")
+        }
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            return ("")
+        }
+            return ("Enter a valid email address.")
+    }
+
+    validatePassword = (password) => {
+        var errorStr = ""
+
+        if(password.length === 0) {
+            errorStr = ""
+            return (<small className="text-danger">{errorStr}</small>)
+        }
+        if (/(?=.{8,})/.test(password) === false) {
+            errorStr += "Password must be at least 8 characters long."
+        }
+        if (/(?=.*[a-z])/.test(password) === false) {
+            errorStr += "\nPassword must contain at least 1 lowercase character."
+        }
+        if (/(?=.*[A-Z])/.test(password) === false) {
+            errorStr += "\nPassword must contain at least 1 uppercase character."
+        }
+        if (/(?=.*[!@#\$%\^&\*])/.test(password) === false) {
+            errorStr += "\nPassword must contain at least one special character."
+        }
+
+        return (<small className="text-danger">{errorStr}</small>)
+    }
+
     handleEmailChange = (event) => {
         this.setState({
-            email: event.target.value
+            email: event.target.value,
+            emailErrorMsg: this.validateEmail(event.target.value)
         })
     }
+
     handlePassChange = (event) => {
         this.setState({
-            password: event.target.value
+            password: event.target.value,
+            passwordErrorMsg: this.validatePassword(event.target.value)
         })
     }
     render() {
-        var headerMessage, btnText;        
-        if(this.props.from === "SignupPage") {
+        var headerMessage, btnText;
+        if (this.props.from === "SignupPage") {
             headerMessage = "Get Started for Free"
             btnText = "Start coding now"
         }
-        else if(this.props.from === "LandingPage") {
+        else if (this.props.from === "LandingPage") {
             headerMessage = "Get Started"
             btnText = "Sign up for free"
         }
@@ -44,18 +83,20 @@ class Signup extends Component {
                         <label className="label label-default font-weight-bold">Email</label>
                         <br />
                         <input type="email" className="form-control" value={this.state.email} onChange={this.handleEmailChange}></input>
+                        <small className="text-danger">{this.state.emailErrorMsg}</small>
                     </div>
                     <div style={{ marginTop: "10px" }}>
                         <label className="label label-default font-weight-bold">Password</label>
                         <br />
                         <input type="password" className="form-control" value={this.state.password} onChange={this.handlePassChange}></input>
+                        <div>{this.state.passwordErrorMsg}</div>
                         <br />
                     </div>
-                    <input type="submit" className="btn btn-primary w-100" style={{height: "3rem", fontSize: "1.25rem"}} value={btnText}></input>
+                    <input type="submit" className="btn btn-primary w-100" style={{ height: "3rem", fontSize: "1.25rem" }} value={btnText}></input>
                 </form>
                 <p className="text-muted">By signing up for StackLearner, you agree to <br />StackLearner's <a href="#">Terms of Service</a> & <a href="#">Privacy Policy</a>.</p>
                 <p className="font-weight-bold">Or, use another account</p>
-                <div className="row" style={{width: "16rem"}}>
+                <div className="row" style={{ width: "16rem" }}>
                     <button className="btn btn-default col col-xs-4"><img src={google_icon} /></button>
                     <button className="btn btn-default col col-xs-4"><img src={fb_icon} /></button>
                     <button className="btn btn-default col col-xs-4"><img src={linkedin_icon} /></button>
